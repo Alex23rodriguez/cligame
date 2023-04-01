e5fb2ad920a095082dab2e153b217d9fa5d9cbea
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-04-01 11:51:45 -0600

    v0.2.2 better docs and flags added to save_raw

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cligame",
-    version="0.2.1",
+    version="0.2.2",
     author="Alex Rodriguez",
     author_email="alex.rodriguez.oro@gmail.com",
     description=short_description,
